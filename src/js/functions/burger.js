93ddb3151a0b6d2195578b1e28ed8d1b33820e09
melnik <melnik@bifit.com>
2022-08-11 19:15:20 +0300

    Fixed burger.js

--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -1,11 +1,12 @@
-import { disableScroll } from 'disable-scroll.js';
-import { enableScroll } from 'enable-scroll';
+import { disableScroll } from './disable-scroll.js';
+import { enableScroll } from './enable-scroll.js';
 
 (function(){
   const burger = document?.querySelector('[data-burger]');
   const menu = document?.querySelector('[data-menu]');
   const menuItems = document?.querySelectorAll('[data-menu-item]');
   const overlay = document?.querySelector('[data-menu-overlay]');
+  const closeBtn = document?.querySelector('[data-menu-close]');
 
   burger?.addEventListener('click', (e) => {
     burger?.classList.toggle('burger--active');
@@ -20,6 +21,14 @@ import { enableScroll } from 'enable-scroll';
       burger?.setAttribute('aria-label', 'Открыть меню');
       enableScroll();
     }
+  });
+
+  closeBtn?.addEventListener('click', () => {
+    burger?.setAttribute('aria-expanded', 'false');
+    burger?.setAttribute('aria-label', 'Открыть меню');
+    burger.classList.remove('burger--active');
+    menu.classList.remove('menu--active');
+    enableScroll();
   });
 
   overlay?.addEventListener('click', () => {
